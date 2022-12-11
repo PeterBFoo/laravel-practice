38cab95c081623a958e2ed6591b4f4f15e3916c6
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-12-11 11:29:41 +0100

    feat: add names to routes

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 Route::get('/blog', function () {
     return view('blog');
-});
+})->name('blog');
 
 // También se puede hacer de esta manera si no hay lógica antes de cargar la vista
 // Route::view('/', 'welcome');
